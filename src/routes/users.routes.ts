1ab4083a0ae2f972c583196fd0616163de2c9329
Enilton Rastele <eniltjr1@gmail.com>
2020-05-05 05:28:35 -0300

    feature: avatar upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvataService from '../services/UpdateUserAvatarService';
+
+import ensureAutheticated from '../middlewares/ensureAuthenticated';
+
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try{
@@ -14,6 +21,21 @@ usersRouter.post('/', async (request, response) => {
     }catch(err){
         return response.status(400).json({ error: err.message })
     }
+});
+
+usersRouter.patch('/', ensureAutheticated, upload.single('file'), async (request, response) => {
+   try {
+
+        const updateUserAvataService = new UpdateUserAvataService();
+        const user = await updateUserAvataService.execute({
+            user_id: request.user.id,
+            avatarFileName: request.file.filename
+        });
+        delete user.password;
+        return response.json(user);
+   }catch (err) {
+        return response.status(400).json({ error: err.message })
+   }
 })
 
 
