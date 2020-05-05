5f5707c2bbee956bcfa7c490005038e4ed6dd386
Enilton Rastele <eniltjr1@gmail.com>
2020-05-05 05:44:04 -0300

    feature:static get avatar file

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,10 @@ import routes from './routes';
 import 'reflect-metadata';
 import './database'
 
+import uploadConfig from './config/upload';
+
 const app = express();
 app.use(express.json());
+app.use('/avatar', express.static(uploadConfig.directory))
 app.use(routes);
 app.listen(3333, () => console.log('Server started at por 3333'));
